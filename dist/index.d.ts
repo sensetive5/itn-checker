4273879e98fae14674fcc6b0c7defcbdf942fde7
Alexander Sokolov <sensetive5@yandex.ru>
2020-06-06 12:51:07 +0300

    Update build

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Функция проверяет корректность ИНН номера
+ * @param {String|Number} innNumber
+ * @return {Boolean}
+ */
+export declare function checkItn(innNumber: string | number): boolean;
